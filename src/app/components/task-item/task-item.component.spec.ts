0030839197cf0d6d13a59539108dcb9af2446640
Jim <vangjim15@gmail.com>
2021-10-02 23:52:15 -0500

    update angular version to 11

--- src/app/components/task-item/task-item.component.spec.ts
+++ src/app/components/task-item/task-item.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { TaskItemComponent } from './task-item.component';
 
@@ -6,7 +6,7 @@ describe('TaskItemComponent', () => {
   let component: TaskItemComponent;
   let fixture: ComponentFixture<TaskItemComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ TaskItemComponent ]
     })
